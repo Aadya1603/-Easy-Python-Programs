b2d19749b6a1bc85fb78aa499a5d81d9a46cc322
Aadya Madanakar <aadyamadankar1099@gmail.com>
2023-02-26 15:50:41 +0530

    Added 1st simple and easy program

---
+++ Working on Variables.py
@@ -0,0 +1,54 @@
+#Variables in Python 
+#Operating on Variables with help of Arithmetic Operations 
+
+a=2
+b=3
+c=a+b
+print(c)
+
+a=2
+b=3
+c=a-b
+print(c)
+
+a=2
+b=3
+c=a*b
+print(c)
+
+a=2
+b=3
+c=a/b
+print(c)
+
+#Some more operations on Variables :
+#Import maths functions :
+
+import math
+a=2
+b=3
+c=a%b
+print(c)
+
+
+a=2
+b=3
+print(math.ceil(a))
+print(math.ceil(b))
+
+
+a=2
+b=3
+print(math.floor(a))
+print(math.floor(b))
+
+
+a=2
+b=3
+print(math.sqrt(a))
+print(math.sqrt(b))
+
+
+
+print(math.pi)
+
