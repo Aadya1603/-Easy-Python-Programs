a5e1f4c26202f2b279149e2daf2eedab5350c16d
Aadya Madanakar <aadyamadankar1099@gmail.com>
2023-02-26 16:09:33 +0530

    Added with specifications

--- Working on Variables.py
+++ Working on Variables.py
@@ -1,21 +1,28 @@
 #Variables in Python 
 #Operating on Variables with help of Arithmetic Operations 
 
+#Addition of Variables
 a=2
 b=3
 c=a+b
 print(c)
 
+
+#Subtraction of Variables 
 a=2
 b=3
 c=a-b
 print(c)
 
+
+#Multiplication of Variables
 a=2
 b=3
 c=a*b
 print(c)
 
+
+#Division of Variables
 a=2
 b=3
 c=a/b
@@ -25,30 +32,33 @@ print(c)
 #Import maths functions :
 
 import math
+
+#Modulus of Variables
 a=2
 b=3
 c=a%b
 print(c)
 
-
+#To check ceil function 
 a=2
 b=3
 print(math.ceil(a))
 print(math.ceil(b))
 
 
+#To check floor function 
 a=2
 b=3
 print(math.floor(a))
 print(math.floor(b))
 
-
+#To check Square Root function 
 a=2
 b=3
 print(math.sqrt(a))
 print(math.sqrt(b))
 
 
-
+#To check Value of PI 
 print(math.pi)
 
